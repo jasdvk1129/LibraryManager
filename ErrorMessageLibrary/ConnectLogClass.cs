729c25bb4050ad8490cd2b13f649401b9453033d
jasdvk1129 <jasdvk1129@gmail.com>
2018-07-20 15:35:56 +0800

    功能新增
    ErrorMessageLibrary版本為1.35.2.0
    1.新增程式運轉狀態顯示功能

---
+++ ErrorMessageLibrary/ConnectLogClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ErrorMessageLibrary
+{
+    public class ConnectLogClass
+    {
+        public ErrorMessageClass ErrMsg = null;
+        public string WorkPath { get; set; }
+        private object _thislock = new object();
+        private Encoding coding = Encoding.Default;
+        public RichTextBox DisplayTextBox;
+
+        public void ConnectStateLogWriter_Text(string _Message)
+        {
+            lock (_thislock)
+            {
+                try
+                {
+                    DateTime NowTime = DateTime.Now;
+                    if (Directory.Exists(WorkPath + "\\Connected") == false)
+                        Directory.CreateDirectory(WorkPath + "\\Connected");
+                    string FileName = WorkPath + "\\Connected\\" + NowTime.ToString("yyyyMMdd") + ".txt";
+                    StreamWriter connectstatefile = new StreamWriter(FileName, true, coding);
+                    connectstatefile.WriteLine(NowTime.ToString("yyyy/MM/dd HH:mm:ss") + "\t\t\t" + _Message);
+                    DisplayTextBox.Text += NowTime.ToString("yyyy/MM/dd HH:mm:ss") + "\t\t" + _Message + Environment.NewLine;
+                    connectstatefile.Close();
+                }
+                catch (Exception ex)
+                {
+                    ErrMsg._errorText("Connect state log error.", ex);
+                }
+            }
+        }
+    }
+}
