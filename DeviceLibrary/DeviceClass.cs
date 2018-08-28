6496c81bc6986a88e9afacaaa65643e5b9287dcb
jasdvk1129 <jasdvk1129@gmail.com>
2018-08-28 16:15:52 +0800

    功能新增
    DeviceLibrary版本為1.2.1.0
    1.新增PA310讀取功能並順道測試繼承
    2.新增DeviceClass設備共用繼承物件

---
+++ DeviceLibrary/DeviceClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeviceLibrary
+{
+    public class DeviceClass
+    {
+        public byte ID { get; set; }                                    //設備ID
+        public byte StartAddrHigh { get; protected set; } = 0x10;       //
+        public byte StartAddrLow { get; protected set; } = 0x01;        //
+        public byte LengthHigh { get; protected set; } = 0x00;          //
+        public byte LengthLow { get; protected set; } = 0x37;           //
+    }
+}
---
+++ DeviceLibrary/PA310Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeviceLibrary
+{
+    public class PA310Class : DeviceClass
+    {
+        public PA310Class()
+        {
+            StartAddrHigh = 0x00;
+            StartAddrLow = 0x00;
+            LengthHigh = 0x00;
+            LengthLow = 0x00;
+        }
+
+        public bool AnalysisDataByte()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            { return false; }
+            return true;
+        }
+    }
+}
