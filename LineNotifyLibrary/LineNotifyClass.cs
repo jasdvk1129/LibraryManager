4e3f7c7a3f2962b630b588a31d276c48b6e7d2be
jasdvk1129 <jasdvk1129@gmail.com>
2018-10-25 14:35:20 +0800

    新增函式庫
    LineNotifyLibrary版本為1.0.0.0
    1.新增LineNotify發送功能的函式庫,函式庫分為有夾帶貼圖發送跟僅有一般文字發送兩種

---
+++ LineNotifyLibrary/LineNotifyClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace LineNotifyLibrary
+{
+    public class LineNotifyClass
+    {
+        public string Token { get; set; }
+        private string ContentType = "application/x-www-form-urlencoded";
+        private string Method = "POST";
+        private string URL = "https://notify-api.line.me/api/notify";
+
+        public string SendLineNotify(string message)
+        {
+            string BearerToken = "Bearer " + Token;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = Method;
+            request.ContentType = ContentType;
+            request.Headers.Add("Authorization", BearerToken);
+            byte[] byteArray = Encoding.UTF8.GetBytes("message=" + HttpUtility.UrlEncode(message));
+            using (Stream reqStream = request.GetRequestStream())
+            {
+                reqStream.Write(byteArray, 0, byteArray.Length);
+            }
+            string responseStr = "";
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        responseStr = sr.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            { responseStr = ex.Message.ToString(); }
+            return responseStr;
+        }
+
+        public string SendLineNotify(string message, int stickerPackageId, int stickerId)
+        {
+            Token = string.Format("Bearer {0}", Token);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = Method;
+            request.ContentType = ContentType;
+            request.Headers.Add("Authorization", Token);
+            NameValueCollection postParams = HttpUtility.ParseQueryString(string.Empty);
+            postParams.Add("stickerPackageId", stickerPackageId.ToString());
+            postParams.Add("stickerId", stickerId.ToString());
+            byte[] byteArray = Encoding.UTF8.GetBytes(HttpUtility.UrlEncode("message=" + message + "&" + postParams.ToString()));
+            using (Stream reqStream = request.GetRequestStream())
+            {
+                reqStream.Write(byteArray, 0, byteArray.Length);
+            }
+            string responseStr = "";
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        responseStr = sr.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            { responseStr = ex.Message.ToString(); }
+            return responseStr;
+        }
+    }
+}
