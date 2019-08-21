c22fcf3337845c9f5dc955b9abaa2c38af86f627
許家榮 <jackeett@gmail.com>
2019-08-21 17:38:14 +0800

    新增Library
    LINEnotifyLibrary(文字傳送、網址圖片傳送)
    TCPClientServer
    (Client : 傳送訊息、檔案
    Server : 接收訊息、檔案)
    SQLSwitchLibrary(SQL服務重啟)

---
+++ SQLSwitchLibrary/SQLswitchClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SQLSwitchLibrary
+{
+    /// <summary>
+    /// 重啟SQLserver服務
+    /// WorkPath = 錯誤檔存入路徑
+    /// 請開啟最高權限使用，點選專案屬性→安全性（勾選"啟用ClickOnce安全設置"），在項目名稱裡的"Properties"下面一個"app.manifest"
+    /// 找尋 level="asInvoker" uiAccess="false"改為 level="requireAdministrator" uiAccess="false" 
+    /// 再(取消勾選"啟用ClickOnce安全設置")，重新建置專案
+    /// </summary>
+    public class SQLswitchClass
+    {
+        /// <summary>
+        /// 錯誤檔存入路徑
+        /// </summary>
+        public string WorkPath { get; set; }
+        /// <summary>
+        /// 重啟SQLserver服務
+        /// </summary>
+        public void SQLserverReStart()
+        {
+            ServiceController controller = new ServiceController();
+            try
+            {
+                controller.MachineName = Environment.MachineName;
+                controller.ServiceName = "MSSQLSERVER";
+                controller.Stop();
+                controller.WaitForStatus(ServiceControllerStatus.Stopped);
+                controller.Start();
+                controller.WaitForStatus(ServiceControllerStatus.Running);
+            }
+            catch (ArgumentNullException ex) { _errorHide("服務名稱請勿輸入NULL", ex); }
+            catch (ArgumentException ex) { _errorHide("確認服務名稱是否正確", ex); }
+            catch (InvalidOperationException ex) { _errorHide("使用者權限錯誤程式請以最高權限執行", ex); }
+        }
+        private object _thislock = new object();
+        private void _errorHide(string errormessage, Exception ex)
+        {
+            lock (_thislock)
+            {
+                DateTime path = DateTime.Now;
+                if (Directory.Exists(WorkPath + "\\error\\" + path.ToString("yyyyMM")) == false)
+                    Directory.CreateDirectory(WorkPath + "\\error\\" + path.ToString("yyyyMM"));
+                string filename = WorkPath + "\\error\\" + path.ToString("yyyyMM") + "\\" + path.ToString("yyyyMMddHHmm") + ".err";
+                StreamWriter errfile = new StreamWriter(filename, true, Encoding.Default);
+                errfile.WriteLine(Directory.GetCurrentDirectory());
+                errfile.WriteLine("時間:" + path.ToString("yyyy/MM/dd HH:mm:ss"));
+                errfile.WriteLine(errormessage);
+                errfile.WriteLine("ex.ToString = " + ex.ToString());
+                errfile.WriteLine("ex.Message = " + ex.Message);
+                errfile.WriteLine("ex.StackTrace = " + ex.StackTrace);
+                errfile.WriteLine("***********************************************************************************************");
+                errfile.WriteLine();
+                errfile.Close();
+            }
+        }
+    }
+}
---
+++ TCPClientServerLibrary/TCPClientServerClass.cs
@@ -0,0 +1,142 @@
+﻿using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static NetworkCommsDotNet.Tools.StreamTools;
+
+namespace TCPClientServerLibrary
+{
+    public class TCPClientServerClass
+    {
+        #region TCPClient
+        /// <summary>
+        /// 傳送訊息
+        /// </summary>
+        /// <param name="Token">權杖</param>
+        /// <param name="IPaddress">IP位址</param>
+        /// <param name="Port">Port號</param>
+        /// <param name="Message">訊息內容</param>
+        public void MessageFunction(string Token, string IPaddress, int Port, string Message)
+        {
+            try
+            {
+                NetworkComms.SendObject(Token, IPaddress, Port, Message);
+            }
+            catch (ArgumentException) { Console.WriteLine("網路通訊異常請檢查"); }
+            catch (ConnectionSetupException) { Console.WriteLine("網路通訊異常請檢查"); }
+            catch (ConnectionSendTimeoutException) { Console.WriteLine("接收端程式異常"); }
+        }
+        /// <summary>
+        /// 傳送檔案
+        /// </summary>
+        /// <param name="Token">權杖</param>
+        /// <param name="IPaddress">IP位址</param>
+        /// <param name="Port">Port號</param>
+        /// <param name="FilePath">檔案路徑</param>
+        public void FileFunction(string Token, string IPaddress, int Port, string FilePath)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+                StreamReader streamReader = new StreamReader(fileStream);
+                DataTable dataTable = new DataTable();
+                bool IsFirst = true;
+                string strline = null;
+                while ((strline = streamReader.ReadLine()) != null)
+                {
+                    if (IsFirst)
+                    {
+
+                        try
+                        {
+                            ThreadSafeStream threadSafeStream = new ThreadSafeStream(fileStream);
+                            StreamSendWrapper sendWrapper = new StreamSendWrapper(threadSafeStream, 0, fileStream.Length);
+                            NetworkComms.SendObject(Token, IPaddress, Port, sendWrapper);
+                            fileStream.Close();
+                        }
+                        catch (ArgumentException) { Console.WriteLine("網路通訊異常請檢查"); }
+                        catch (ConnectionSetupException) { Console.WriteLine("網路通訊異常請檢查"); }
+                        catch (ConnectionSendTimeoutException) { Console.WriteLine("接收端程式異常"); }
+                    }
+                    break;
+                }
+            }
+            catch (FileNotFoundException) { Console.WriteLine("搜尋不到或未有檔案產生"); }
+            catch (ObjectDisposedException) { Console.WriteLine("檔案刪除後未有檔案產生"); }
+            catch (DirectoryNotFoundException) { Console.WriteLine("找不到檔案路徑"); }
+        }
+        #endregion
+
+        #region TCPServer
+        /// <summary>
+        /// 訊息內容
+        /// </summary>
+        public string MessageString { get; set; } = null;
+        /// <summary>
+        /// 檔案儲存位址
+        /// </summary>
+        public string FilePath { get; set; }
+        /// <summary>
+        /// 辦別Token
+        /// </summary>
+        public string Tokennum { get; set; }
+        /// <summary>
+        /// 顯示IP位址與PORT
+        /// </summary>
+        public string IPstring { get; set; }
+        /// <summary>
+        /// 開啟TCP連線
+        /// </summary>
+        /// <param name="Port">設定PORT</param>
+        public void ConnectionTCP(int Port)
+        {
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Any, Port));
+            foreach (System.Net.IPEndPoint localEndPoint in Connection.ExistingLocalListenEndPoints(ConnectionType.TCP))
+            {
+                IPstring += "IP位址: " + localEndPoint.Address + "\r\n Port: " + localEndPoint.Port.ToString() + "\r\n";
+            }
+            WebClient webClient = new WebClient();
+            string publicIP = webClient.DownloadString("https://api.ipify.org");
+            IPstring += "外部IP:" + publicIP + "\r\n Port:" + Port;
+        }
+        #region 接收訊息解析
+        /// <summary>
+        /// 接收訊息
+        /// </summary>
+        /// <param name="Token">權杖</param>
+        public void MessageFunction(string Token)
+        {
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>(Token, PrintIncomingMessage);
+        }
+        private void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+            MessageString = message;
+            Tokennum = header.PacketType;
+        }
+        #endregion
+        #region 接收檔案解析
+        /// <summary>
+        /// 接收檔案
+        /// </summary>
+        /// <param name="Token">權杖</param>
+        public void FileFunction(string Token)
+        {
+            NetworkComms.AppendGlobalIncomingPacketHandler<byte[]>(Token, FieldLoad);
+        }
+        private void FieldLoad(PacketHeader packetHeader, Connection connection, byte[] incomingData)
+        {
+            FileStream fileStream = File.Create(FilePath, incomingData.Length);
+            fileStream.Write(incomingData, 0, incomingData.Length);
+            fileStream.Close();
+            Tokennum = packetHeader.PacketType;
+        }
+        #endregion
+        #endregion
+    }
+}
