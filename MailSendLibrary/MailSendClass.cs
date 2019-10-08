c649dbbcc7503e3de91eef6817696ad97082248c
jasdvk1129 <jasdvk1129@gmail.com>
2019-10-08 14:06:18 +0800

    修正並優化資料庫物件,修正mail發送物件.合併SQL服務偵測重啟物件於資料庫處禮物件中

--- MailSendLibrary/MailSendClass.cs
+++ MailSendLibrary/MailSendClass.cs
@@ -2,14 +2,37 @@
 using System.Text;
 using System.Net;
 using System.Net.Mail;
-using ErrorMessageLibrary;
-using System.IO;
+using Serilog;
 
 namespace MailSendLibrary
 {
     public class MailSendClass
     {
-        ErrorMessageClass ErrMsg;
+        public MailSendClass()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}\\maillog\\log-.txt",
+                          rollingInterval: RollingInterval.Day,
+                          outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();        //宣告Serilog初始化
+        }
+        /// <summary>
+        /// 基本Mail傳送物件函式
+        /// </summary>
+        /// <param name="mSenderID">寄件者</param>
+        /// <param name="mSenderName">寄件者名稱</param>
+        /// <param name="mTo">收件者</param>
+        /// <param name="mCC">副本</param>
+        /// <param name="mBCC">密件副本</param>
+        /// <param name="mSubject">主旨</param>
+        /// <param name="mBody">內文</param>
+        /// <param name="mVerificationID">Mail Server 帳號</param>
+        /// <param name="mVerificationPwd">Mail Server 密碼</param>
+        /// <param name="mSMTPPort">SMTP Port</param>
+        /// <param name="mSMTPServer">SMTP Server</param>
+        /// <param name="mSSL">SSL加密</param>
+        /// <returns></returns>
         public bool TheEmailSend(string mSenderID, string mSenderName,
                                  string mTo, string mCC, string mBCC,
                                  string mSubject, string mBody,
@@ -19,8 +42,6 @@ namespace MailSendLibrary
         {
             try
             {
-                ErrMsg = new ErrorMessageClass();
-                ErrMsg._WorkPath = Directory.GetCurrentDirectory();         //錯誤訊息儲存路徑
                 MailMessage myMail = new MailMessage();
                 myMail.From = new MailAddress(mSenderID, mSenderName);
                 if (!(mTo.Trim().Length == 0))
@@ -39,11 +60,12 @@ namespace MailSendLibrary
                 mySmtp.Host = mSMTPServer;
                 mySmtp.EnableSsl = mSSL;
                 mySmtp.Send(myMail);
+                Log.Information($"Send email success,from {mSenderID} to {mTo} use smtp setting is {mSMTPServer}:{mSMTPPort} SSL {mSSL.ToString()}");
                 return true;
             }
             catch (Exception ex)
             {
-                ErrMsg._error("Send email message error.", ex);
+                Log.Error(ex, $"Send email message failed, from {mSenderID} to {mTo} use smtp setting is{mSMTPServer}:{mSMTPPort} SSL {mSSL.ToString()}");
                 return false;
             }
         }
