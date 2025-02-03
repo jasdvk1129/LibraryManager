339056f8783a5ae35d1a4a669776841c83574637
Keith <jasdvk1129@gmail.com>
2025-02-03 14:29:50 +0800

    新增簡訊發報支援群睿簡訊機的通訊

--- SMSLibrary/SMSClass.cs
+++ SMSLibrary/SMSClass.cs
@@ -189,6 +189,184 @@ namespace SMSLibrary
         }
         #endregion
 
+        #region 群睿科技_簡訊發送
+        public void Gainwise_SMS_Send(string TelNo, string Message)
+        {
+            if (Rs232.IsOpen)
+            {
+                string _TelNo = TelNo.Trim();
+                string _Message = Message.Trim();
+                string Buff_Message = Message.Trim();
+                string SmsCenterNo = "";        // 簡訊中心號碼
+                List<string> Send_Messgae = new List<string>(); ;
+                if (!_TelNo.All(char.IsDigit))
+                {
+                    Log.Error("電話號碼中有非數字");
+                    return;
+                }
+                else
+                {
+                    if (_TelNo.Length != 12 & _TelNo.Length == 10)
+                    {
+                        _TelNo = "886" + $"{_TelNo.Substring(1, 9)}";
+                    }
+                }
+                while (Buff_Message.Length > 0)
+                {
+                    if (Buff_Message.Length > 70)
+                    {
+                        Send_Messgae.Add(Buff_Message.Substring(0, 70));
+                        Buff_Message = Buff_Message.Remove(0, 70);
+                    }
+                    else
+                    {
+                        Send_Messgae.Add(Buff_Message.Substring(0, Buff_Message.Length));
+                        Buff_Message = Buff_Message.Remove(0, Buff_Message.Length);
+                    }
+                }
+                foreach (var Messageitem in Send_Messgae)
+                {
+                    byte[] InByte0 = new byte[Rs232.BytesToRead];
+                    int ReadCount0 = Rs232.Read(InByte0, 0, Rs232.BytesToRead);
+
+                    byte[] cmd1 = Encoding.ASCII.GetBytes("AT+CSCA?" + "\r");   // 第一道命令，抓簡訊中心號碼
+                    Rs232.Write(cmd1, 0, cmd1.Length);
+                    Thread.Sleep(500);
+                    byte[] InByte1 = new byte[Rs232.BytesToRead];
+                    int ReadCount1 = Rs232.Read(InByte1, 0, Rs232.BytesToRead);
+                    if (ReadCount1 == 0)
+                    {
+                        Log.Error("Error,簡訊機無回應");
+                        return;
+                    }
+                    else
+                    {
+                        string _TempStr = Encoding.ASCII.GetString(InByte1, 0, InByte1.Length);
+                        if (_TempStr.IndexOf("OK") < -1)
+                        {
+                            Log.Error($"Error,{_TempStr}");
+                            return;
+                        }
+                        else
+                        {
+                            int Index = _TempStr.IndexOf("+CSCA: \"+");
+                            if (Index < -1)
+                            {
+                                Log.Error($"Error,找不到 簡訊中心號碼({_TempStr})");
+                                return;
+                            }
+                            else
+                            {
+                                SmsCenterNo = _TempStr.Substring(Index + 10 - 1, 12);
+                                if (!SmsCenterNo.All(char.IsDigit))
+                                {
+                                    Log.Error($"Error,簡訊中心號碼異常({_TempStr})");
+                                    return;
+                                }
+                            }
+                        }
+                    }
+
+                    byte[] cmd2 = Encoding.ASCII.GetBytes("AT+CMGF=0" + "\r");
+                    byte[] cmd3 = Encoding.ASCII.GetBytes("AT+CMGS=" + $"{Messageitem.Length * 2 + 14}" + "\r");
+                    byte[] cmd4 = new byte[46 + Messageitem.Length * 4];
+                    // 079188961300009911000C91
+                    // 0123456789 123456789 123
+                    // 0791 好像是固定
+                    // 889613000099 是 簡訊中心號碼 +886931000099(遠傳) (查詢命令 AT+CSCA? )
+                    // 中華電信的簡訊中心電話號碼有3個(886932400821,886932400841,886932400851)
+                    // 11000C91 好像是固定
+                    string T1Str = "0791";      // 好像固定
+                    for (int i = 0; i < T1Str.Length; i++)//長度4
+                    {
+                        cmd4[i] = Encoding.ASCII.GetBytes(T1Str.Substring(i, 1))[0];
+                    }
+                    for (int i = 0; i < SmsCenterNo.Length; i += 2)//解析簡訊中心電話號碼 長度12
+                    {
+                        cmd4[i + 4] = Encoding.ASCII.GetBytes(SmsCenterNo.Substring(i + 1, 1))[0];
+                        cmd4[i + 5] = Encoding.ASCII.GetBytes(SmsCenterNo.Substring(i, 1))[0];
+                    }
+                    string T2Str = "11000C91";      // 好像固定
+                    //              12345678
+                    for (int i = 0; i < T2Str.Length; i++)//長度8
+                    {
+                        cmd4[i + 16] = Encoding.ASCII.GetBytes(T2Str.Substring(i, 1))[0];
+                    }
+                    for (int i = 0; i < _TelNo.Length; i += 2)//解析電話號碼 長度12
+                    {
+                        cmd4[i + 24] = Encoding.ASCII.GetBytes(_TelNo.Substring(i + 1, 1))[0];
+                        cmd4[i + 25] = Encoding.ASCII.GetBytes(_TelNo.Substring(i, 1))[0];
+                    }
+                    string T3Str = "0008AA";      // 好像固定
+                    //              123456
+                    for (int i = 0; i < T3Str.Length; i++)//長度6
+                    {
+                        cmd4[i + 36] = Encoding.ASCII.GetBytes(T3Str.Substring(i, 1))[0];
+                    }
+                    string EndStr = (Messageitem.Length * 2).ToString("X2");            // 訊息內容長度
+                    for (int i = 0; i < EndStr.Length; i++)//長度2
+                    {
+                        cmd4[i + 42] = Encoding.ASCII.GetBytes(EndStr.Substring(i, 1))[0];
+                    }
+                    char[] Messagedata = Messageitem.ToCharArray();
+                    for (int i = 0; i < Messageitem.Length; i++)
+                    {
+                        string Temp = Convert.ToInt32(Messagedata[i]).ToString("X4");
+                        cmd4[43 + i * 4 + 1] = Encoding.ASCII.GetBytes(Temp.Substring(0, 1))[0];
+                        cmd4[43 + i * 4 + 2] = Encoding.ASCII.GetBytes(Temp.Substring(1, 1))[0];
+                        cmd4[43 + i * 4 + 3] = Encoding.ASCII.GetBytes(Temp.Substring(2, 1))[0];
+                        cmd4[43 + i * 4 + 4] = Encoding.ASCII.GetBytes(Temp.Substring(3, 1))[0];
+                    }
+                    cmd4[44 + Messageitem.Length * 4] = 26;
+
+                    Rs232.Write(cmd2, 0, cmd2.Length);
+                    Thread.Sleep(200);
+                    byte[] InByte2 = new byte[Rs232.BytesToRead];
+                    int ReadCount2 = Rs232.Read(InByte2, 0, Rs232.BytesToRead);
+                    if (ReadCount2 == 0)
+                    {
+                        Log.Error("Error,簡訊機無回應");
+                    }
+                    else
+                    {
+                        string _TempStr = Encoding.ASCII.GetString(InByte2, 0, InByte2.Length);
+                        if (_TempStr.IndexOf("OK") < -1)
+                        {
+                            Log.Error($"Error,{_TempStr}");
+                        }
+                        else
+                        {
+                            //Log.Error($"{_TempStr}");
+                        }
+                    }
+
+                    Rs232.Write(cmd3, 0, cmd3.Length);
+                    Thread.Sleep(50);
+                    Rs232.Write(cmd4, 0, cmd4.Length);
+                    Thread.Sleep(2000);
+                    byte[] InByte3 = new byte[Rs232.BytesToRead];
+                    int ReadCount3 = Rs232.Read(InByte3, 0, Rs232.BytesToRead);
+                    if (ReadCount3 == 0)
+                    {
+                        Log.Error("Error,簡訊機無回應");
+                    }
+                    else
+                    {
+                        string _TempStr = Encoding.ASCII.GetString(InByte3, 0, InByte3.Length);
+                        if (_TempStr.IndexOf("ERROR") > 0)
+                        {
+                            Log.Error($"Error,{_TempStr}");
+                        }
+                        else
+                        {
+                            //Log.Error($"OK,{_TempStr}");
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
         #region 釋放
         private bool _disposed = false;
         private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
